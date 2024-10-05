ab0417bb5dc9bedb1e595466a62921cd8beb7726
Ben Pearce <me@benpearce.io>
2024-10-06 00:39:47 +0100

    feat: :children_crossing: skip version increment in bubblewrap script

--- scripts/bubblewrap-build.sh
+++ scripts/bubblewrap-build.sh
@@ -2,7 +2,7 @@ for file in /app-manifests/*.json; do
     mkdir -p /app/$(basename $file .json)
     ln -sf $file /app/$(basename $file .json)/twa-manifest.json
     cd /app/$(basename $file .json)
-    bubblewrap update
+    bubblewrap update --skipVersionUpgrade
     echo y | bubblewrap build --signingKeyPath /repo/keystore.p12 --signingKeyAlias $KEY_ALIAS
     fingerprint=$(keytool -list -keystore /repo/keystore.p12 -storetype PKCS12 -storepass $KEYSTORE_PASSWORD  | grep -A 1 'SHA-256' | sed 's/.*(SHA-256): //')
     bubblewrap fingerprint --manifest=$(pwd)/twa-manifest.json add $fingerprint
